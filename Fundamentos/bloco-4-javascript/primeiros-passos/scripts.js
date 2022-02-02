c2a6e6642bd7ee39c61326a355fd735676ec75a8
Amanda Zambelli <amandazambelli@ymail.com>
2022-02-02 19:40:22 -0300

    até exercicio 7

---
+++ Fundamentos/bloco-4-javascript/primeiros-passos/scripts.js
@@ -0,0 +1,117 @@
+//const a = 5;
+//const b = 10;
+//const c = 7;
+
+// console.log("Soma: " + (a + b));
+// console.log("Subtração: " + (a - b));
+//console.log("Divisão: " + (a / b));
+//console.log("Multiplicação: " + (a * b));
+// console.log("Módulo: " + (a % b));
+
+
+//if (a > b) {
+//    console.log("a é maior que b")
+//}
+
+//else {
+//    console.log("b é maior que a")
+//}
+
+
+//if (a > b && a > c) {
+//    console.log("a é o maior número");
+//}
+
+//else if (b > a && b > c) {
+//    console.log("b é o maior número");
+//}
+
+//else {
+//    console.log("c é o maior número");
+//}
+
+//const valor = 7;
+
+//if (valor > 0) {
+//    console.log("Positivo");
+//}
+
+//else if (valor < 0) {
+//    console.log("Negativo");
+//}
+
+//else {
+//    console.log("0");
+//}
+
+
+//let anguloUm = 60;
+//let anguloDois = 60;
+//let anguloTres = 60;
+
+//angulosPositivos = anguloUm > 0 && anguloDois > 0 && anguloTres > 0
+
+//somaAngulos = anguloUm + anguloDois + anguloTres
+
+//if (angulosPositivos) {
+//  if (somaAngulos === 180) {
+//    console.log(true);
+// } else {
+//    console.log(false);
+//  }
+//
+//} else {
+//    console.log('Erro: ângulo inválido');
+//}
+
+
+//let peca = "cavalo";
+
+//switch (peca.toLowerCase()) {
+//  case "rei":
+//    console.log("Rei, ande apenas uma casa");
+//    break;
+//  case "torre":
+//    console.log("Torre, ande lateral");
+//    break;
+//  case "rainha":
+//    console.log("Rainha, ande como quiser");
+//    break;
+//  case "bispo":
+//    console.log("Bispo, ande na diagonal");
+//    break;
+//  case "peao":
+//    console.log("Peão, ande duas casas");
+//    break;
+//  case "cavalo":
+//    console.log("Cavalo, ande em L");
+//    break;
+//  default:
+//    console.log("Peça inválida");
+//    break;
+//}
+
+
+const nota = 66;
+
+If (nota < 0 || nota > 100) {
+  console.log("erro");
+} 
+else if (nota >= 90) {
+  console.log("A");
+} 
+else if (nota >= 80) {
+  console.log("B");
+} 
+else if (nota >= 70) {
+  console.log("C");
+} 
+else if (nota >= 60) {
+  console.log("D");
+} 
+else if (nota >= 50) {
+  console.log("E");
+} 
+else if (nota < 50) {
+  console.log("F");
+}
