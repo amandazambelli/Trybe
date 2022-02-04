2d77d316dbd5c9301f604e6678ebc9e8079e3c79
Amanda Zambelli <amandazambelli@ymail.com>
2022-02-04 18:30:10 -0300

    exercicios dia 3 do bloco 4

---
+++ Fundamentos/bloco-4-javascript/logica-programacao/scripts.js
@@ -0,0 +1,90 @@
+/* EX FRUTAS
+
+let fruits = [3, 4, 10, 1, 12];
+
+let valorFinal = 0;
+
+for (index = 0; index < fruits.length; index += 1) {
+    valorFinal += fruits[index];
+}
+
+    if (valorFinal > 15) {
+        console.log(valorFinal);
+    } else {
+        console.log("16");
+    }*/
+
+
+    //EXERCICIO 1 - FATORIAL DE 10
+
+    //let fatorial = 1;
+
+    //for (index = 10; index > 0; index -= 1) {
+    //    console.log(fatorial *= [index]);
+    //}
+
+//console.log(fatorial);
+
+//EXERCICIO 2 - TROCAR ORDEM LETRAS
+
+//let word = 'tryber';
+
+
+// Faça um algoritmo que calcule a soma de 1 a 50 usando a estrutura "for" e retorne no formato:
+// A soma total de 1 a 50 é: '
+
+/*soma = 0;
+
+for (index = 1; index <=50; index +=1) {
+    soma += index;
+}
+
+console.log('A soma total de 1 a 50 é: ' + soma);*/
+
+
+
+// 5 
+
+//Crie um algoritmo que recebe a idade de Carolzita , Murilo e Baêta e imprime quem é a pessoa mais nova no formato:
+// "Pessoa" é a mais nova.
+
+/*let carolzita = 10;
+let murilo = 20;
+let baeta = 3;
+
+if (carolzita < murilo && carolzita < baeta) {
+    console.log("Carolzita é a mais nova!")
+} else if (murilo < carolzita && murilo < baeta) {
+    console.log("Murilo é o mais novo!")
+} else {
+    console.log("Baeta é o mais novo!")
+}*/
+
+
+//4
+//Desenvolva um algoritmo que verifica se a pessoa é maior ou menor de idade.
+//Imprima no console seguindo o exemplo: "A pessoa é maior de idade".
+//Bônus: Crie a condição utilizando operador ternário.
+
+/*let pessoa = 20;
+
+
+if (pessoa >= 18) {
+    console.log("A pessoa é maior de idade");
+} else {
+    console.log("A pessoa é menor de idade");
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
