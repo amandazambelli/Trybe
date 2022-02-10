0fc39a983a8f9636d3712174b3eb249d36c6402b
Amanda Zambelli <amandazambelli@ymail.com>
2022-02-10 12:02:52 -0300

    adicionando

--- Fundamentos/bloco-4-javascript/objetos-funcoes/script.js
+++ Fundamentos/bloco-4-javascript/objetos-funcoes/script.js
@@ -101,3 +101,39 @@ if (leitor.livrosFavoritos.length >= 2) {
 } else {
     console.log(leitor.nome + " tem " + leitor.livrosFavoritos.length + " ivro favorito.")
 }
+
+
+
+/////// FUNCOES ///////////
+
+/*1 - Crie uma função que receba uma string e retorne true se for um palíndromo , 
+ou false , se não for.
+
+Exemplo de palíndromo: arara .
+verificaPalindrome('arara') ;
+Retorno esperado: true
+verificaPalindrome('desenvolvimento') ;
+Retorno esperado: false*/
+
+function verificaPalindrome(string) {
+  let reverse = string.split('').reverse().join('');
+  if (reverse === string) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(verificaPalindrome('arara'));
+
+
+//2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+//Array de teste: [2, 3, 6, 7, 10, 1]; .
+//Valor esperado no retorno da função: 4 .
+
+
+
+
+
+
+ 
